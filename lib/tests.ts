83ebf5f885070b1f2ce6afff2f6541c6f3b5d3b2
Steve Thompson <writetome51@gmail.com>
2018-11-25 22:28:00 -0700

    began adding tests

---
+++ lib/tests.ts
@@ -0,0 +1,168 @@
+import { PublicArraySorter } from './index';
+import { arraysMatch } from '@writetome51/arrays-match';
+
+
+let sort = new PublicArraySorter();
+let otherArr = sort.data;
+
+
+// Test 1
+sort.data = ['book', 'copy', 'zaa', 'aac', '_10', 100, 'aaa', 'atco',
+	'Zebra', 'bat', '__100', '!', 'aab', '@', 'Ã', '#', '$', '%', '^',
+	'&', '*', '000', 'AE', '(', ')', '-', 'Atco', '+', '=', 'aardvark', 2,
+	'1000', '30', 0, 'A', 'qqq', '~', 'x', 'Ô', 'Â', '<', '>', ',', '.', 'Í', 'Ǣ',
+	'i', '/', '?', 'Zaa', '∏', '|', '\\'];
+let obj = sort.alphabetize();
+
+if (arraysMatch(sort.data, [
+	'__100', '_10', '-', ',', '!', '?', '.', '(', ')', '@', '*', '/', '\\', '&', '#',
+	'%', '^', '∏', '+', '<', '=', '>', '|', '~', '$', 0, '000', 100, '1000',
+	2, '30', 'A', 'Â', 'Ã', 'aaa', 'aab', 'aac', 'aardvark', 'AE', 'Ǣ', 'Atco',
+	'atco', 'bat', 'book', 'copy', 'i', 'Í', 'Ô', 'qqq', 'x', 'Zaa', 'zaa', 'Zebra'
+])) console.log('test 1 passed');
+else console.log('test 1 FAILED');
+
+
+// Test 1A: make sure function returned the class instance:
+if (obj.className && obj.className === 'PublicArraySorter' &&
+	arraysMatch(sort.data, obj.data)) console.log('test 1A passed');
+else console.log('test 1A FAILED');
+
+
+//Test 2
+if (arraysMatch(sort.data, otherArr)) console.log('test 2 passed');
+else console.log('test 2 FAILED');
+
+
+// Test 2A: test .adjacentAt and make sure function returned the class instance:
+obj = sort.adjacentAt(0, [10, 20, 30, 40]);
+if (arraysMatch(sort.data, [10, 20, 30, 40, 5, 6, 7, 8, 9, '100']) &&
+	(obj.className && obj.className === 'PublicArraySorter' &&
+		arraysMatch(sort.data, obj.data))) console.log('test 2A passed');
+else console.log('test 2A FAILED');
+
+
+//Test 2B
+if (arraysMatch(sort.data, otherArr)) console.log('test 2B passed');
+else console.log('test 2B FAILED');
+
+
+// Test 3: test .adjacentToValue and make sure function returned the class instance:
+sort.data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+otherArr = sort.data;
+obj = sort.adjacentToValue({value: 4, howMany: 3, offset: 1}, [11, 12, 13]);
+if (arraysMatch(sort.data, [1, 2, 3, 4, 11, 12, 13, 8, 9]) &&
+	(obj.className && obj.className === 'PublicArraySorter' &&
+		arraysMatch(sort.data, obj.data))) console.log('test 3 passed');
+else console.log('test 3 FAILED');
+
+
+//Test 4: make sure memory reference wasn't broken:
+if (arraysMatch(sort.data, otherArr)) console.log('test 4 passed');
+else console.log('test 4 FAILED');
+
+
+// Test 5
+obj = sort.between(3, ['hello', 'there']);
+if (arraysMatch(sort.data, [1, 2, 3, 'hello', 'there', 13, 8, 9]) &&
+	(obj.className && obj.className === 'PublicArraySorter' &&
+		arraysMatch(sort.data, obj.data))) console.log('test 5 passed');
+else console.log('test 5 FAILED');
+
+
+//Test 6
+if (arraysMatch(sort.data, otherArr)) console.log('test 6 passed');
+else console.log('test 6 FAILED');
+
+
+// Test 7
+sort.data = [1, 2, 3, 'hello', 'there', 13, 8, 9, 3];
+otherArr = sort.data;
+obj = sort.firstOf(3, [1]);
+if (arraysMatch(sort.data, [1, 2, [1], 'hello', 'there', 13, 8, 9, 3]) &&
+	(obj.className && obj.className === 'PublicArraySorter' &&
+		arraysMatch(sort.data, obj.data))) console.log('test 7 passed');
+else console.log('test 7 FAILED');
+
+
+//Test 8
+if (arraysMatch(sort.data, otherArr)) console.log('test 8 passed');
+else console.log('test 8 FAILED');
+
+
+// Test 9
+sort.data = [1, 2, 1, 3, 'hello', 'there', 2, 13, 8, 9, 3];
+otherArr = sort.data;
+obj = sort.firstOfEach([1, 2, 3], ['h', 'i', 'j']);
+if (arraysMatch(sort.data, ['h', 'i', 1, 'j', 'hello', 'there', 2, 13, 8, 9, 3]) &&
+	(obj.className && obj.className === 'PublicArraySorter' &&
+		arraysMatch(sort.data, obj.data))) console.log('test 9 passed');
+else console.log('test 9 FAILED');
+
+
+//Test 10
+if (arraysMatch(sort.data, otherArr)) console.log('test 10 passed');
+else console.log('test 10 FAILED');
+
+
+// Test 11
+sort.data = [1, 2, 1, 3, 'hello', 'there', 2, 13, 8, 9, 3];
+otherArr = sort.data;
+obj = sort.allOf(2, '');
+if (arraysMatch(sort.data, [1, '', 1, 3, 'hello', 'there', '', 13, 8, 9, 3]) &&
+	(obj.className && obj.className === 'PublicArraySorter' &&
+		arraysMatch(sort.data, obj.data))) console.log('test 11 passed');
+else console.log('test 11 FAILED');
+
+
+//Test 12
+if (arraysMatch(sort.data, otherArr)) console.log('test 12 passed');
+else console.log('test 12 FAILED');
+
+
+// Test 13
+sort.data = [1, 2, 1, 3, 'hello', 'there', 2, 13, 8, 9, 3];
+otherArr = sort.data;
+obj = sort.allOfEach([1, 2, 3], ['h', 'i', 'j']);
+if (arraysMatch(sort.data, ['h', 'i', 'h', 'j', 'hello', 'there', 'i', 13, 8, 9, 'j']) &&
+	(obj.className && obj.className === 'PublicArraySorter' &&
+		arraysMatch(sort.data, obj.data))) console.log('test 13 passed');
+else console.log('test 13 FAILED');
+
+
+//Test 14
+if (arraysMatch(sort.data, otherArr)) console.log('test 14 passed');
+else console.log('test 14 FAILED');
+
+
+// Test 15
+sort.data = [1, 2, 1, 3, 'hello', 'there', 2, 13, 8, 9, 3];
+otherArr = sort.data;
+obj = sort.each((item, index, array) => {
+	if (index > 5) return (item * 2);
+	else return item;
+});
+if (arraysMatch(sort.data, [1, 2, 1, 3, 'hello', 'there', 4, 26, 16, 18, 6]) &&
+	(obj.className && obj.className === 'PublicArraySorter' &&
+		arraysMatch(sort.data, obj.data))) console.log('test 15 passed');
+else console.log('test 15 FAILED');
+
+
+//Test 16
+if (arraysMatch(sort.data, otherArr)) console.log('test 16 passed');
+else console.log('test 16 FAILED');
+
+
+// Test 17
+sort.data = [1, 2, 1, 3, 'hello', 'there', 2, 13, 8, 9, 3];
+otherArr = sort.data;
+obj = sort.allWithOne([13, 'hello', 'there'], 0);
+if (arraysMatch(sort.data, [1, 2, 1, 3, 0, 0, 2, 0, 8, 9, 3]) &&
+	(obj.className && obj.className === 'PublicArraySorter' &&
+		arraysMatch(sort.data, obj.data))) console.log('test 17 passed');
+else console.log('test 17 FAILED');
+
+
+//Test 18
+if (arraysMatch(sort.data, otherArr)) console.log('test 18 passed');
+else console.log('test 18 FAILED');
